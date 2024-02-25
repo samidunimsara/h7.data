33e3d88d11805e90e6c920333c08b771f3bab7d2
Samidu Nimsara <95490856+samidunimsara@users.noreply.github.com>
2024-02-25 13:18:15 +0530

    Create awscertsub.sh

---
+++ awscertsub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+API_KEY="pUI72s8WEv4dpIVPLQQHWa4uPD0Qgz5E2TWIzQMH"
+API_URL="https://tls.bufferover.run/dns?q=."
+
+read -p "Enter domain name or list of domains (separated by spaces): " input_domains
+
+IFS=' ' read -ra domains <<< "$input_domains"
+
+for domain in "${domains[@]}"; do
+    
+    query_url="${API_URL}${domain}"
+
+    result=$(curl -s "$query_url" -H "x-api-key:$API_KEY" | jq -r '.Results[] | split(",")[4]' | sort -u)
+
+    output_file="${domain}_result.txt"
+    echo "$result" > "$output_file"
+
+    echo "result for $domain saved to $output_file"
+done
